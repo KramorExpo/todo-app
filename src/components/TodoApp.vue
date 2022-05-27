e001db57f1f7189b11bd2ba80cacb579419a6dd5
kramorexpo <kramorexpo@gmail.com>
2022-05-27 11:28:41 +0300

    first

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,103 @@
+<template>
+<div class = "container ">
+  <h2 class="text-center mt-5">Todo App</h2>
+
+  <div class="d-flex">
+    <input v-model="task" type="text" placeholder = "Enter task" class="form-control">
+    <button @click="submitTask" class="btn button-warning rounded-0">SUBMIT</button>
+  </div>
+
+  <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Status</th>
+      <th scope="col" class = "text-center">#</th>
+      <th scope="col" class = "text-center">#</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(task , index) in tasks" :key="index">
+      <td>{{task.name}}</td>
+      <td style="width: 120px"><span @click = "changeStatus(index)" class = "pointer">{{task.status}}</span></td>
+      <td>
+        <div class = "text-center" @click="editTask(index)">
+          <span class="fa fa-pen"></span>
+        </div>
+        </td>
+      <td>
+        <div class = "text-center" @click="deleteTask(index)">
+          <span class = "fa fa-trash"></span>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      task: "Hello world",
+      editedTask: null,
+      availableStatus: ["to-do","in-progress", "finished"],
+      tasks: [
+        {
+          name: "steal bananas",
+          status: "to-do"
+        },
+         {
+          name: "eat bananas",
+          status: "to-do"
+        }
+      ]
+    }
+  },
+
+  methods: {
+    submitTask(){
+      if(this.task.length === 0) return;
+
+      if(this.editedTask === null) {
+        this.tasks.push({
+          name: this.task,
+          status: "to-do"
+        })
+      } else {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      }
+      
+      this.task = " ";
+    },
+
+    deleteTask(index){
+      this.tasks.splice(index, 1);
+    },
+
+    editTask(index){
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+
+    changeStatus(index) {
+        let newindex = this.availableStatus.indexOf(this.tasks[index].status)
+        if(++newindex > 2) newindex = 0
+        this.tasks[index].status = this.availableStatus[newindex]
+    },
+  }
+}
+</script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+</style>
+
